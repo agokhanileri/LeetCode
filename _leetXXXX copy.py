9b1d17b975a617b35f721ba062fb2808a48c9f34
agi <alexgileri@outlook.com>
2023-09-05 22:12:53 -0700

    test push

---
+++ _leetXXXX copy.py
@@ -0,0 +1,27 @@
+# 0XXX. XXXX
+# 
+
+# ----------------------------------------------------------------------
+# Clarifications:
+# If one digit? --> Palindrome
+# Can pad with 0s s.t. 120 & 021? --> No, not Palindrome
+# Negative number? --> Can't be Palindrome
+
+# Inputs:
+x = 
+
+# ----------------------------------------------------------------------
+# Sol1: O() / O()
+
+
+# ----------------------------------------------------------------------
+# Submit: Sol1, O() / O()
+class Solution:
+    def 
+    
+        return 
+
+# --------------
+# Test:
+sol = Solution()
+print(sol.YYYY(x))
